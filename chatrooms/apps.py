080e6e2d53e5d300a58c11dc96a921f920b57223
Mohammad Murtaza Naimi <45477322+MMnaimi@users.noreply.github.com>
2023-04-13 12:14:15 +0430

    Update apps.py

--- chatrooms/apps.py
+++ chatrooms/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ChatroomsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chatrooms'
+    name = 'chatrooms;lkfjas;dkflj;lsdkjf;lsfj sadfjsa d;kfl s;dkjf slfdj'
